ef7e061b6bfa210a3c4aaf99d6b6a6b9541c87a7
a89504725766-cell <a89504725766@gmail.com>
2026-03-01 12:39:21 +0500

    Initialize main.jsx with ReactDOM rendering

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import GembaWalk from '../gemba-mobile.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <GembaWalk />
+  </React.StrictMode>
+)
