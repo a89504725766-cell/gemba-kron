0e177424f7faef3ed2e702fd1704d955524a0be2
a89504725766-cell <a89504725766@gmail.com>
2026-03-01 13:23:35 +0500

    Rename GembaWalk to GembaApp and change file extension

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import GembaWalk from '../gemba-mobile.jsx'
+import GembaApp from '../gemba-mobile.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <GembaWalk />
+          <GembaApp />
   </React.StrictMode>
 )
